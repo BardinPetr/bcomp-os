09f64a490b87c02dbd49bb0f1bcc234596ad9660
BardinPetr <bardin.petr@gmail.com>
2022-11-28 02:40:22 +0300

    P_LAB4: some docs

--- programming/year_1/lab_4/app/src/main/java/ru/bardinpetr/itmo/lab_4/realitylib/story/Story.java
+++ programming/year_1/lab_4/app/src/main/java/ru/bardinpetr/itmo/lab_4/realitylib/story/Story.java
@@ -10,6 +10,32 @@ import ru.bardinpetr.itmo.lab_4.realitylib.things.PhysicalObject;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class is a base class for building a story.
+ * Any story part should extend this class. Any sub-story should be a child of Main story.
+ * It is intended not to interact with its internal structure.
+ * The right way is to properly dislocate all dependencies and annotate your class:
+ * All reused things should be in class's fields.
+ * Any field that is used by at least two stories should be injected from parent story using DI.
+ * When you create some field with object you want to include in the scene, annotate it with @StoryProvide
+ * To inject field use @StoryInject. It will match by field name.
+ * Types in DI are matched automatically by checking cast availability.
+ * To add some code to set up environment (like mods) create any method and annotate it with @SetupMethod
+ * To add scenario, create field for it, initialize with scenario object and create method annotated with @CreateScenario
+ * To add pure abilities to IAble objects use @Able annotation.
+ * To include a sub-story put it into fields, initialize and annotate as @StoryProvides
+ * Startup sequence is the following:
+ * 1. the main class is called with .compile()
+ * 2. all @StoryProvides in this class is evaluated and store in Maps in Story
+ * 2.1. StoryProvides also connects substories to current
+ * 2.2. The main story loads all providers of sub
+ * 2.3. The injects of sub is fulfilled from main
+ * 2.4. substory is compiled (this list is a compile process)
+ * 3. @Able annotations is processed for current class
+ * 4. All setup methods of current class are called sequentially
+ * 5. All scenario methods of current class are called sequentially
+ * 6. The End!
+ */
 public abstract class Story {
     private final String storyName;
     private final Map<String, Human> actors = new HashMap<>();
